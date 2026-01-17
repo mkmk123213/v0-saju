8faa00529d41c24bf4a4fd497eff7bfe242e91f4
mkmk123213 <kimgodyd@gmail.com>
2026-01-18 01:51:45 +0900

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,8 +3,10 @@ import { createClient } from "@supabase/supabase-js"
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
-console.log("ENV CHECK", {
+  
+  console.log("ENV CHECK", {
   url: process.env.NEXT_PUBLIC_SUPABASE_URL,
   hasAnon: !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 })
+)
+
