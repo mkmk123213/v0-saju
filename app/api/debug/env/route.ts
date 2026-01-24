fdb58c9b20e2f6ab807ec0e30e1a521a8f5aaad9
mkmk123213 <kimgodyd@gmail.com>
2026-01-24 23:40:31 +0900

    Delete app directory

--- app/api/debug/env/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const service = process.env.SUPABASE_SERVICE_ROLE_KEY ?? "";
-  return NextResponse.json({
-    SUPABASE_SERVICE_ROLE_KEY: { present: !!service, len: service.length },
-    VERCEL_ENV: process.env.VERCEL_ENV ?? null,
-  });
-}
